d33b9cfe5de55ababa701c92e44c87107b8dd7a1
kyogo.matsuo <kyogo.matsuo@gmail.com>
2020-06-27 19:06:21 +0200

    Add TopPage and EncodingFilter

---
+++ src/filters/EncodingFilter.java
@@ -0,0 +1,49 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    // doFilterメソッドで、フィルターとしての実行内容を定義。
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        //サーブレットが処理を実行する前にフィルタ処理を実行
+
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+}
