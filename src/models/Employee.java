b9828553b2dcb894497d98f53182382a7a2baf1a
kyogo.matsuo <kyogo.matsuo@gmail.com>
2020-07-01 17:38:26 +0200

    Add Employee show

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -25,7 +25,7 @@ import javax.persistence.Table;
 
     @NamedQuery(
             name = "checkRegisteredCode",
-            query = "SELECT e FROM Employee AS e WHERE e.code = :code"
+            query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
             ),
     @NamedQuery(
             name = "checkLoginCodeAndPassword",
