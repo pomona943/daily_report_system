c0007a1da42174762fbb408d6f1dc1c175440e51
kyogo.matsuo <kyogo.matsuo@gmail.com>
2020-07-09 06:44:57 +0200

    Add Report update

---
+++ src/controllers/reports/ReportsUpdateServlet.java
@@ -0,0 +1,80 @@
+package controllers.reports;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Report;
+import utils.DBUtil;
+import validators.ReportValidator;
+
+/**
+ * Servlet implementation class ReportsUpdateServlet
+ */
+@WebServlet("/reports/update")
+public class ReportsUpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReportsUpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // edit.jspで_tokenのname属性で送られてきた値の確認。
+        // _tokenがnull出なく、且つセッションスコープ内のデータと同じだった場合に、emを作成。
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // 編集する日報1件を取得する。
+            Report r = em.find(Report.class, (Integer)(request.getSession().getAttribute("report_id")));
+
+            // フォームの内容を各プロパティに上書き。
+            r.setReport_date(Date.valueOf(request.getParameter("report_date")));
+            r.setTitle(request.getParameter("title"));
+            r.setContent(request.getParameter("content"));
+            r.setUpdated_at(new Timestamp(System.currentTimeMillis()));
+
+            List<String> errors = ReportValidator.validate(r);
+            if(errors.size() > 0) {
+                em.close();
+
+                // エラーがあれば、
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("report", r);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
+                rd.forward(request, response);
+            } else {
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                em.close();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+
+                request.getSession().removeAttribute("report_id");
+
+                response.sendRedirect(request.getContextPath() + "/reports/index");
+            }
+
+        }
+
+    }
+
+}
